c90e90504bf58a5ca150bddeafe906b8b03b0e25
Kumar Harsh <kumarharsh232001@gmail.com>
2026-06-04 23:03:20 +0530

    Implemented Kafka to Spark Bronze Layer streaming pipeline

---
+++ spark_jobs/kafka_to_bronze.py
@@ -0,0 +1,35 @@
+from pyspark.sql import SparkSession
+
+spark = (
+    SparkSession.builder
+    .appName("KafkaToBronze")
+    .getOrCreate()
+)
+
+spark.sparkContext.setLogLevel("WARN")
+
+kafka_df = (
+    spark.readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "payment-kafka:29092")
+    .option("subscribe", "transactions_raw")
+    .option("startingOffsets", "latest")
+    .load()
+)
+
+raw_df = kafka_df.selectExpr(
+    "CAST(value AS STRING) as json_data"
+)
+
+query = (
+    raw_df.writeStream
+    .format("parquet")
+    .option("path", "/app/bronze")
+    .option("checkpointLocation", "/app/bronze_checkpoint")
+    .outputMode("append")
+    .start()
+)
+
+print("Bronze Streaming Started...")
+
+query.awaitTermination()
